c76a572cd7c05039d3f10955bf1cc172a7677f7a
ant1k9 <ant1k9@protonmail.com>
2019-11-11 00:53:31 +0300

    Fix nohup commands.

--- provisioning/start_services.sh
+++ provisioning/start_services.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nohup go run ../cmd/observer/main.go
-nohup go run ../cmd/notificator/main.go
-nohup go run ../cmd/server/main.go
+nohup go run ../cmd/observer/main.go &
+nohup go run ../cmd/notificator/main.go &
+nohup go run ../cmd/server/main.go &
