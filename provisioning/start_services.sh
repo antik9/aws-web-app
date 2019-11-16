f4e9cbd138f9febee62f8f04be0642f816ecb375
ant1k9 <ant1k9@protonmail.com>
2019-11-16 19:16:18 +0300

    Remove RabbitMQ from provisioning, disable notificator on start

--- provisioning/start_services.sh
+++ provisioning/start_services.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /etc/environment
 nohup go run ../cmd/observer/main.go &
-nohup go run ../cmd/notificator/main.go &
+# nohup go run ../cmd/notificator/main.go &
 nohup go run ../cmd/server/main.go &
